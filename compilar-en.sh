a14767cf56133133379887e81a70282b0b214b72
Arturo Candela <acandela@cipfpbatoi.es>
2023-01-19 07:01:31 +0100

    Cambiados los scripts en bash por Makefile, eliminadas el resto de versiones en otros idiomas, quitada la configuración de hyperref y integrada de nuevo en el archivo curriculum. También se ha renombrado el archivo bib por simplicidad

--- compilar-en.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Compilar Ingles:"
-
-echo "\tCurriculum Imprimible"
-./compilar.sh curriculum-en hypersetup.imprimible.tex curriculum-en-imprimible >/dev/null
-
-echo "\tCurriculum On Line"
-./compilar.sh curriculum-en hypersetup.online.tex curriculum-en-online >/dev/null
--- compilar-es.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Compilar Castellano:"
-
-echo "\tCurriculum Imprimible"
-./compilar.sh curriculum-es hypersetup.imprimible.tex curriculum-es-imprimible > /dev/null
-
-echo "\tCurriculum On Line"
-./compilar.sh curriculum-es hypersetup.online.tex curriculum-es-online >/dev/null
